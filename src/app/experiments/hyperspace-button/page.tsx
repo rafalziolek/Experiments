9ad62df0b67a6fadfb1cc3d24a8dd0a5fa2d1a84
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2024-11-25 22:18:58 +0100

    Update page.tsx

--- src/app/experiments/hyperspace-button/page.tsx
+++ src/app/experiments/hyperspace-button/page.tsx
@@ -7,9 +7,9 @@ export default function page() {
       <div className="right-10 fixed bg-stone-900 top-10 p-4 rounded-xl max-w-80 text-white">
         <h3 className="font-sans font-semibold text-sm">🚧 This is work in progress</h3>
         <p className="font-sans text-sm pt-6">
-          This button doesn't do anything when you click it. It's just a
+          This button doesn't do anything when you click it (yet). It's just a
           component that looks cool. </p>
-          <p className="font-sans text-sm pt-3">The goal is to use three.js for effect and animation.</p>
+          <p className="font-sans text-sm pt-3">The goal is to use three.js for hover effect and animation after clicking</p>
         </div>
     </div>
   );
